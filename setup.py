c75118d0f83183f8014f74d2301a7afb2b1af4a3
Kitchai Chaomuangchai <j_ckitchai@outlook.com>
2024-06-04 02:00:03 +0700

    setup installed

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages,setup
+
+
+setup(
+    name = "Homecredit",
+    version = "0.0.0",
+    author = "Kitchai",
+    author_email = "J_ckitchai@outlook.com",
+    packages= find_packages()
+)
